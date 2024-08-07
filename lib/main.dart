e4f6197effeadb8085ecdbfe9cf30ab68abe973e
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-08-07 18:05:38 +0200

    about page

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyHomePage extends StatelessWidget {
           children: [
             Center(child: Text('Home Page')),
             Center(child: Text('Location Page')),
-            Center(child: Text('About Page')),
+            AboutPage(),
           ],
         ),
         bottomNavigationBar: Container(
@@ -48,6 +48,39 @@ class MyHomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'About Forecast Friend',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'This is an app developed for the course 1DV535 at Linneaus University. '
+            'It was built using Flutter and Open Weather Map API and was '
+            'developed by Alambuya Chelsea. '
+            'Forecast Friend provides you with the latest weather updates in '
+            'your current and saved location to help you stay prepared.',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
       ),
     );
   }
