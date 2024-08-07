5aa71e90f15ac4f86ad294a6121d1b66fb3f9d8c
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-08-07 18:47:09 +0200

    saved towns read from file and carousel created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'home_tab.dart';
+import 'search_tab.dart';
+import 'about_tab.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,9 +36,9 @@ class MyHomePage extends StatelessWidget {
         ),
         body: const TabBarView(
           children: [
-            Center(child: Text('Home Page')),
-            Center(child: Text('Location Page')),
-            AboutPage(),
+            HomeTab(),
+            SearchTab(),
+            AboutTab(),
           ],
         ),
         bottomNavigationBar: Container(
@@ -43,44 +46,11 @@ class MyHomePage extends StatelessWidget {
           child: const TabBar(
             tabs: [
               Tab(icon: Icon(Icons.home), text: 'Home'),
-              Tab(icon: Icon(Icons.location_on), text: 'Location'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
               Tab(icon: Icon(Icons.info), text: 'About'),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  const AboutPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'About Forecast Friend',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 16),
-          Text(
-            'This is an app developed for the course 1DV535 at Linneaus University. '
-            'It was built using Flutter and Open Weather Map API and was '
-            'developed by Alambuya Chelsea. '
-            'Forecast Friend provides you with the latest weather updates in '
-            'your current and saved location to help you stay prepared.',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 15),
-          ),
-        ],
       ),
     );
   }
